be259724570e19ec4532fa6344bc834cdf1e01f6
Paul Sherer <wolfymaster@hotmail.com>
2020-08-10 16:53:36 -0400

    pretty layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const songs = [
+  {
+    "title": "Song 1",
+    "coverart": "https://picsum.photos/seed/song1/300",
+    "playcount": 2,
+    "url": "https://www.youtube.com/watch?v=eof02pn4KtM"
+  },
+  {
+    "title": "Song 2",
+    "coverart": "https://picsum.photos/seed/song2/300",
+    "playcount": 15,
+    "url": "https://www.youtube.com/watch?v=YnmOmNqBWtM"
+  },
+  {
+    "title": "Song 3",
+    "coverart": "https://picsum.photos/seed/song3/300",
+    "playcount": 8,
+    "url": "https://www.youtube.com/watch?v=MEkaqZecpUQ"
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Disaster Demo", songs: songs });
 });
 
 module.exports = router;
