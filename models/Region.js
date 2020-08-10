6e06c1ed1714a05e3ee62f1ec6ab9563bc08aabb
Paul Sherer <wolfymaster@hotmail.com>
2020-08-10 17:44:27 -0400

    add models

---
+++ models/Region.js
@@ -0,0 +1,14 @@
+  
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const Region = sequelize.define('Region', {
+    user_id: DataTypes.STRING,
+    region: DataTypes.STRING
+  }, {
+    timestamps: false
+  })
+  Region.associate = function(models) {
+    // associations can be defined here
+  }
+  return Region
+}
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-const songs = [
-  {
-    "title": "Song 1",
-    "coverart": "https://picsum.photos/seed/song1/300",
-    "playcount": 2,
-    "url": "https://www.youtube.com/watch?v=eof02pn4KtM"
-  },
-  {
-    "title": "Song 2",
-    "coverart": "https://picsum.photos/seed/song2/300",
-    "playcount": 15,
-    "url": "https://www.youtube.com/watch?v=YnmOmNqBWtM"
-  },
-  {
-    "title": "Song 3",
-    "coverart": "https://picsum.photos/seed/song3/300",
-    "playcount": 8,
-    "url": "https://www.youtube.com/watch?v=MEkaqZecpUQ"
-  }
-]
+const db = require('../models/')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Disaster Demo", songs: songs });
+  res.render('login', {})
 });
+
+router.get('/dashboard', async (req, res) => {
+  let rows = await queryWeatherData()
+  let userRows = await queryUserData()
+  let regionRows = await queryRegionData()
+  let userObject = {
+      name: req.user.email
+  }
+  res.render('dashboard', { data: rows, userData: userRows[0] || {}, regionData: regionRows, userSession: userObject })
+
+  async function queryRegionData() {
+      let user_id = req.user.id
+
+      let data = await db.Region.findAll({
+          where: {
+              user_id: user_id
+          }
+      })
+
+      return data
+  }
+
+  async function queryWeatherData() {
+
+      let data = await db.Weather.findAll()
+
+      return data
+  }
+
+  async function queryUserData() {
+      let user_id = req.user.id
+
+      let data = await db.Settings.findOrCreate({
+          where: {
+              user_id: user_id
+          }
+      })
+
+      return data
+  }
+})
 
 module.exports = router;
