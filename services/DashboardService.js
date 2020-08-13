4a18fb155cd0f2600279692b2bdd59eb6fb997ed
Paul Sherer <wolfy@wolfymaster.com>
2020-08-13 18:57:02 -0400

    updates

--- services/DashboardService.js
+++ services/DashboardService.js
@@ -1,20 +1,16 @@
 const User = require('../lib/user');
-const Weather = require('../lib/weather');
 
 class DashboardService {
     constructor(db, userid) {
         this.db = db;
         this.user = new User(db, userid)
-        this.weather = new Weather(db);
     }
 
     async params() {
         let userDetails = await this.user.userDetails;
-        let weatherData = await this.weather.data;
         let regionData = await this.user.regionData;
 
         return {
-            data: weatherData,
             userData: userDetails || {}, 
             regionData: regionData
         }
